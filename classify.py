c694305d7fc93cd91a9d287f2b39e72c4f5900f7
dodiku <dda290@nyu.edu>
2017-04-08 17:07:28 -0400

    init

---
+++ classify.py
@@ -0,0 +1,14 @@
+'''
+Machine Learning for Cities // New York University
+Instructor: Professor Luis Gustavo Nonato
+
+Written by: Dror Ayalon
+'''
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import naive_bayes
+from sklearn import svm
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
